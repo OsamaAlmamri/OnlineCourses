f2b25aecf3b9d33f83264de77bc95b4b8aaeb691
NohaAlwajeeh <59328495+NohaAlwajeeh@users.noreply.github.com>
2020-01-27 10:59:38 +0300

    edit style nabvbar

--- app/view/admin/includes/navbar.php
+++ app/view/admin/includes/navbar.php
@@ -1,32 +1,42 @@
-</head>
-<body>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <!-- Brand -->
 
     <div class="container-fluid">
         <div class="navbar-header">
-            <i class></i> <a class="navbar-brand" href="#">
+           <i class></i> <a class="navbar-brand" href="#" >
                 <img src="/image/download.png" class="rounded-circle" width="25">
                 <label class="hrefLabel">Dashboard</label></a>
 
         </div>
-        <!-- Links -->
+    <!-- Links -->
 
-        <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="ion ion-email font-size-24" ></i>
+                <span class="badge badge-light">4</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="ion ion-android-notifications font-size-24" ></i>
+                <span class="badge badge-light">4</span>
+            </a>
+        </li>
+        <!-- Dropdown -->
+        <li class="nav-item dropdown " >
+            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+               <img src="/image/boss.png"  class="rounded-circle" width="25"> Admin Name
+            </a>
 
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#"><i class="ion ion-edit"></i> Edit Profile</a>
+                <a class="dropdown-item" href="#"><i class="ion ion-log-out"></i> Logout</a>
 
-            <!-- Dropdown -->
-            <li class="nav-item dropdown ">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                    <img src="/image/boss.png" class="rounded-circle" width="25"> Admin Name
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#"><i class="ion ion-edit"></i> Edit Profile</a>
-                    <a class="dropdown-item" href="/home/logout"><i class="ion ion-log-out"></i> Logout</a>
+            </div>
+        </li>
 
-                </div>
-            </li>
-        </ul>
+    </ul>
 
 </nav>
 <div class="sidebar" id="mySidenav">
@@ -35,13 +45,12 @@
     </div>
     <div class="link">
 
-        <a href="/admin/index"><i class="fa fa-home iconi"></i> <label class="hrefLabel">Home</label></a>
+    <a href="/admin/home/index"><i class="fa fa-home iconi"></i> <label class="hrefLabel">Home</label></a>
         <div class="panel-group">
             <div class="panel panel-default">
                 <div class="panel-heading">
 
-                    <a data-toggle="collapse" href="#collapse1"><i class="fa fa-users-cog iconi"></i> <label
-                                class="hrefLabel">Accounts<i class="fa fa-angle-down "></i></label></a>
+                        <a data-toggle="collapse" href="#collapse1"><i class="fa fa-users-cog iconi"></i> <label class="hrefLabel">Accounts<i class="fa fa-angle-down " ></i></label></a>
 
                 </div>
                 <div id="collapse1" class="panel-collapse collapse">
@@ -49,13 +58,12 @@
                         <a href="#"><i class="fa fa-user-cog icon"></i> <label class="hrefLabel">Sub Admins</label></a>
 
 
+
                     </div>
                     <div class="panel-footer">
-                        <a href="/admin/universities/index"><i class="ion ion-university icon"></i> <label
-                                    class="hrefLabel">Universities</label></a>
-                        <a href="/admin/teachers/index"><i class="fa fa-chalkboard-teacher icon"></i> <label
-                                    class="hrefLabel">Teachers</label></a>
-                    </div>
+                        <a href="/admin/universities/index"><i class="ion ion-university icon"></i> <label class="hrefLabel">Universities</label></a>
+                        <a href="/admin/teachers/index"><i class="fa fa-chalkboard-teacher icon"></i> <label class="hrefLabel">Teachers</label></a>
+                        </div>
                 </div>
             </div>
         </div>
@@ -63,23 +71,22 @@
             <div class="panel panel-default">
                 <div class="panel-heading">
 
-                    <a data-toggle="collapse" href="#collapse2"><i class="fa fa-users-cog iconi"></i> <label
-                                class="hrefLabel">Manages<i class="fa fa-angle-down "></i></label></a>
+                        <a data-toggle="collapse" href="#collapse2"><i class="fa fa-users-cog iconi"></i> <label class="hrefLabel">Manages<i class="fa fa-angle-down " ></i></label></a>
 
                 </div>
                 <div id="collapse2" class="panel-collapse collapse">
                     <div class="panel-body">
-                        <a href="#"><i class="fa fa-user-cog icon"></i> <label class="hrefLabel">Roles</label></a>
-                        <a href="#"><i class="fa fa-user-cog icon"></i> <label class="hrefLabel">Permision</label></a>
-                    </div>
+                        <a href="role.php"><i class="fa fa-user-cog icon"></i> <label class="hrefLabel">Roles</label></a>
+                        <a href="permision.php"><i class="fa fa-cogs icon"></i> <label class="hrefLabel">Permision</label></a>
+
+                     </div>
 
                 </div>
             </div>
         </div>
 
-        <a href="/admin/courses/index"><i class="fa fa-book-reader iconi"></i> <label class="hrefLabel">Courses</label></a>
-        <a href="/admin/categories/index"><i class="fa fa-list iconi"></i> <label
-                    class="hrefLabel">Categories</label></a>
+        <a href="courses.php"><i class="fa fa-book-reader iconi"></i> <label class="hrefLabel">Courses</label></a>
+        <a href="categories.php"><i class="fa fa-list iconi"></i> <label class="hrefLabel">Categories</label></a>
 
     </div>
 </div>
