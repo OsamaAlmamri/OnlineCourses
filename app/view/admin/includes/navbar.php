8bb8fa4e6f2b6ae801826a385201cc549d0a30a6
OsamaAlmamri <osama1234887@gmail.com>
2020-02-04 09:23:36 +0300

    Revert "Merge branch 'taher'"
    This reverts commit 1ccbcf8cb13dea398ccd9e3fbbeb762183534a31, reversing
    changes made to bc96c925419e578ba9601d7946e37b2146cf30f2.

--- app/view/admin/includes/navbar.php
+++ app/view/admin/includes/navbar.php
@@ -1,121 +1,101 @@
-</head>
-<div class="container-fluid">
-    <div class="row">
-        <!--  Start Sidebar -->
-        <div class="col-sm-2 p-0 ">
-            <div class="sidebar bg-dark" id="mySidenav">
-                    <div class="logo-site pr-3">
-                        <img  src="/image/logo.png" >
-                    </div>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand -->
 
-                <div class="userImage text-center">
-                    <img src="<?php echo(Session::get('user')['user_image']) ?>" class="rounded-circle" width="30">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <i class></i> <a class="navbar-brand" href="#">
+                <img src="/image/download.png" class="rounded-circle" width="25">
+                <label class="hrefLabel">Dashboard</label></a>
+
+        </div>
+        <!-- Links -->
+
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="ion ion-email font-size-24"></i>
+                    <span class="badge badge-light">4</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="ion ion-android-notifications font-size-24"></i>
+                    <span class="badge badge-light">4</span>
+                </a>
+            </li>
+            <!-- Dropdown -->
+            <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+
+                    <img src="<?php echo(Session::get('user')['user_image']); ?>" class="rounded-circle"
+                         width="25"> <?php echo(Session::get('user')['user_name']) ?>
+                </a>
+
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#"><i class="ion ion-edit"></i> Edit Profile</a>
+                    <a class="dropdown-item" href="/admin/logout"><i class="ion ion-log-out"></i> Logout</a>
+
                 </div>
-                <div class="link m-5">
-                    <a href="/admin/index"><i class="fa fa-home iconi"></i> <label class="hrefLabel">الصفحة الرئيسية</label></a>
-                    <div class="panel-group">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <a data-toggle="collapse" href="#collapse1"><i class="fa fa-users-cog iconi"></i> <label
-                                            class="hrefLabel">الحسابات<i class="fa fa-angle-down "></i></label></a>
-                            </div>
-                            <div id="collapse1" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                    <a href="#">
-                                        <i class="fa fa-user-cog icon"></i>
-                                        <label class="hrefLabel">المدراء الفرعين </label>
-                                    </a>
+            </li>
+
+        </ul>
+
+</nav>
+<div class="sidebar" id="mySidenav">
+    <div class="image_back">
+        <img src="<?php echo(Session::get('user')['user_image']) ?>" class="rounded-circle" alt="Cinque Terre"
+             width="30">
+    </div>
+    <div class="link">
+
+        <a href="/admin/index"><i class="fa fa-home iconi"></i> <label class="hrefLabel">Home</label></a>
+        <div class="panel-group">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+
+                    <a data-toggle="collapse" href="#collapse1"><i class="fa fa-users-cog iconi"></i> <label
+                                class="hrefLabel">Accounts<i class="fa fa-angle-down "></i></label></a>
+
+                </div>
+                <div id="collapse1" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <a href="#"><i class="fa fa-user-cog icon"></i> <label class="hrefLabel">Sub Admins</label></a>
 
 
-                                </div>
-                                <div class="panel-footer">
-                                    <a href="/admin/universities/index"><i class="ion ion-university icon"></i> <label
-                                                class="hrefLabel">الجامعات</label></a>
-                                    <a href="/admin/teachers/index"><i class="fa fa-chalkboard-teacher icon"></i> <label
-                                                class="hrefLabel">الأساتذة</label></a>
-                                </div>
-                            </div>
-                        </div>
                     </div>
-                    <div class="panel-group">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-
-                                <a data-toggle="collapse" href="#collapse2"><i class="fa fa-users-cog iconi"></i> <label
-                                            class="hrefLabel">الادارة<i class="fa fa-angle-down "></i></label></a>
-
-                            </div>
-                            <div id="collapse2" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                    <a href="/admin/role/index"><i class="fa fa-user-cog icon"></i> <label
-                                                class="hrefLabel">الادوار</label></a>
-                                    <a href="/admin/permision/index"><i class="fa fa-cogs icon"></i> <label
-                                                class="hrefLabel">الصلاحيات</label></a>
-
-                                </div>
-
-                            </div>
-                        </div>
+                    <div class="panel-footer">
+                        <a href="/admin/universities/index"><i class="ion ion-university icon"></i> <label
+                                    class="hrefLabel">Universities</label></a>
+                        <a href="/admin/teachers/index"><i class="fa fa-chalkboard-teacher icon"></i> <label
+                                    class="hrefLabel">Teachers</label></a>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="panel-group">
+            <div class="panel panel-default">
+                <div class="panel-heading">
 
-                    <a href="/admin/courses/index"><i class="fa fa-book-reader iconi"></i> <label class="hrefLabel">الكورسات</label></a>
-                    <a href="/admin/categories/index"><i class="fa fa-list iconi"></i> <label
-                                class="hrefLabel">الأقسام</label></a>
+                    <a data-toggle="collapse" href="#collapse2"><i class="fa fa-users-cog iconi"></i> <label
+                                class="hrefLabel">Manages<i class="fa fa-angle-down "></i></label></a>
+
+                </div>
+                <div id="collapse2" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <a href="/admin/role/index"><i class="fa fa-user-cog icon"></i> <label
+                                    class="hrefLabel">Roles</label></a>
+                        <a href="/admin/permision/index"><i class="fa fa-cogs icon"></i> <label
+                                    class="hrefLabel">Permision</label></a>
+
+                    </div>
 
                 </div>
             </div>
         </div>
-        <!--  End Sidebar   -->
 
-        <!-- Start Content -->
-        <div class="col-sm-10 p-0">
+        <a href="/admin/courses/index"><i class="fa fa-book-reader iconi"></i> <label class="hrefLabel">Courses</label></a>
+        <a href="/admin/categories/index"><i class="fa fa-list iconi"></i> <label
+                    class="hrefLabel">Categories</label></a>
 
-            <!-- Start navBar -->
-            <div class="row-cols-1 mb-4">
-                <div class="mynav">
-                    <nav class="navbar navbar-expand-sm bg-dark navbar-dark ">
-                        <!-- Brand -->
-                        <div class="col-6 text-right">
-<!--                            <a class="text-white" href="#"> لوحة التحكم</a>-->
-                        </div>
-                        <!-- Links -->
-                        <div class="col-6 text-left">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <i class="ion ion-email font-size-24"></i>
-                                        <span class="badge badge-light">4</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item ">
-                                    <a class="nav-link" href="#">
-                                        <i class="ion ion-android-notifications font-size-24"></i>
-                                        <span class="badge badge-light">4</span>
-                                    </a>
-                                </li>
-                                <!-- Dropdown -->
-                                <li class="nav-item dropdown ">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                                        <img src="<?php echo(Session::get('user')['user_image']); ?>" class="rounded-circle"
-                                             width="25px"> <?php echo(Session::get('user')['user_name']) ?>
-                                    </a>
-
-                                    <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="#"><i class="ion ion-edit"></i> تعديل الملف الشخصي </a>
-                                        <a class="dropdown-item" href="/admin/logout"><i class="ion ion-log-out"></i> تسجيل
-                                            الخروج</a>
-                                    </div>
-                                </li>
-
-                            </ul>
-
-                        </div>
-                    </nav>
-                </div>
-            </div>
-
-            <!-- End navBar -->
-
-
-
-
+    </div>
+</div>
