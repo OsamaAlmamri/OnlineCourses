626bb5633ea2ae0421dad9c15a8dc5c816bbbd7c
OsamaAlmamri <osama1234887@gmail.com>
2020-02-10 17:52:03 +0300

    solve some merge problem

--- app/controller/site/coursesController.php
+++ app/controller/site/coursesController.php
@@ -2,49 +2,50 @@
 
 
 namespace site;
-use auth\Activation;
-use auth\login;
-use auth\LogOut;
-use auth\Register;
+
 use Session;
-use Controller;
 
 /* ** */
+
 class coursesController extends \Controller
 {
 
 
+    public function old()
+    {
+
+
+        //return var_dump($_COOKIE['item_id']);
+    }
+
     public function index()
     {
-        $course_site=$this->model('Course_site');
-        $this->view('website' . DIRECTORY_SEPARATOR . 'courses', ['courses' => $course_site->all() ,'deleted' => false]);
-        $this->view->pageTitle ='Course_site';
-        $this->view->render();
-}
-    public function featchLatestCourses()
-    {
-        $course_site=$this->model('Course_site');
 
-        $this->view('website' . DIRECTORY_SEPARATOR . 'home', ['homeCourses' => $course_site->latestCoursesWebsite(),'deleted' => false]);
-        $this->view->pageTitle ='Home';
+        $course_site = $this->model('Course_site');
+        $this->view('website' . DIRECTORY_SEPARATOR . 'courses', ['courses' => $course_site->all(), 'deleted' => false]);
+        $this->view->pageTitle = 'Course_site';
         $this->view->render();
     }
 
-    public function insertCourseUser(){
-   $userCourse=$this->model('Course_site');
-    $userInfoCourse=array(
-        ":user_id" =>Session::get('user')['user_id'],
+    public function insertCourseUser()
+    {
+
+        $userCourse = $this->model('Course_site');
+        $userInfoCourse = array(
+            ":user_id" => Session::get('user')['user_id'],
 //   ":couces_buy"=>implode(",",$_COOKIE['item_id']),
-        ":user_wish_list"=>implode(",",$_COOKIE['item_whishlish_id'])
-    );
-   $userCourse->insertInToUsersCourses($userInfoCourse);
-echo 1;
-   // Helper::back('/cousres' , 'add successfully', 'success');
+            ":user_wish_list" => implode(",", $_COOKIE['item_whishlish_id'])
+        );
+        $userCourse->insertInToUsersCourses($userInfoCourse);
+        echo 1;
+        // Helper::back('/cousres' , 'add successfully', 'success');
 
     }
 
 //public function addCoursesId(){
 //    Session::set('cousresUserId'=>);
 //}
+
 }
+
 ?>
