536cabe07fd96b61edf384e4dfae8529efe73ecb
OsamaAlmamri <osama1234887@gmail.com>
2020-02-26 12:11:52 +0300

    whishlist

--- app/controller/site/wishListController.php
+++ app/controller/site/wishListController.php
@@ -3,6 +3,7 @@
 namespace site;
 
 use Controller;
+use Helper;
 use Session;
 
 class wishListController extends Controller
@@ -15,13 +16,24 @@ class wishListController extends Controller
         $course_site = $this->model('Course_site');
         $user_id = Session::get('user')['user_id'];
         $WishList = $course_site->wishListUser($user_id)[0]['user_wish_list'];
-        $courses = $course_site->wishListElements('(' . trim($WishList, ",") . ')');
+        $whereIn=($WishList!='')? '(' . trim($WishList, ",") . ')':'(0)';
+        $courses = $course_site->wishListElements($whereIn);
         if (($WishList) != '') {
             $userWishList = (explode(',', $WishList));
         } else
             $userWishList = [];
+        $lastCourses = [];
+        $this->model('Lesson');
+        foreach ($courses as $course) {
+            $lastCourses[] = array_merge($course, $this->course_info($course['course_id']));
+        }
+//        $this->view('website' . DIRECTORY_SEPARATOR . 'index', [
+//            'courses' => $lastCourses,
+//            'userWishList' => $userWishList,
+//
+//        ]);
         $this->view('website' . DIRECTORY_SEPARATOR . 'wish_list',
-            ['courses' => $courses,
+            ['courses' => $lastCourses,
                 'userWishList' => $userWishList,
                 'deleted' => false,
             ]);
@@ -29,6 +41,47 @@ class wishListController extends Controller
         $this->view->render();
 
     }
+    public function course_info($id)
+    {
+//        $lessons = $this->model=null;
+        $lessons = $this->model;
+        $courseDuration = 0;
+        $chaptersLessons = [];
+        $course_count = $lessons->count_lessons($id);
+        $chaptersName = $lessons->chapterNames($id);
+        foreach ($chaptersName as $chapter) {
+            $videos = $lessons->chapterLessons($id, $chapter['resources_chapter']);
+            $chapterDuration = $this->countDurationChapter($videos);
+            $courseDuration += $chapterDuration;
+            $chaptersLessons[$chapter['resources_chapter']] = array(
+                'duration' => gmdate("H:i:s", $chapterDuration),
+                'lessons' => $videos,
+            );
+        }
+        $averageRating = $this->model->averageRating($id);
+        $info = array(
+            'duration' => gmdate("H:i:s", $courseDuration),
+            'course_count' => $course_count,
+            'averageRating' => isset($averageRating[0]['average_rating']) ? $averageRating[0]['average_rating'] : 0,
+            'AllRatings' => isset($averageRating[0]['total_rating']) ? $averageRating[0]['total_rating'] : 0,
+        );
+        return $info;
+    }
+    public function countDurationChapter($data)
+    {
+        $sum = 0;
+
+        foreach ($data as $lesson) {
+
+
+            $file = Helper::getVideoDeatils(substr($lesson['resources_video'], 1));
+            $sum += $file['playtime_seconds'];
+        }
+        return $sum;
+
+
+    }
+
 }
 
 ?>
